6c83f85ca13e85a86fe4c95b81d6855830347165
Rafael Delanhese Machado <hepta.rafael.d@AGU.GOV.BR>
2020-03-02 13:58:48 -0300

    Primeiro commit

---
+++ src/main/java/com/delanhese/corretora/CorretoraApplication.java
@@ -0,0 +1,13 @@
+package com.delanhese.corretora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorretoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorretoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delanhese/corretora/CorretoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delanhese.corretora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorretoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
